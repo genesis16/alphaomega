b4646e66acf4f8a424eb0e3ac0a412220f9f8b33
Jane James <janeleeshian@gmail.com>
2019-08-27 09:50:37 +1000

    change case

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../components/card'
+import Card from '../components/Card'
 import Section from '../components/section'
 import Wave from '../components/wave'
 import Layout from '../layouts'
